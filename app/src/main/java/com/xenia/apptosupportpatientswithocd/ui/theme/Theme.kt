dd6d0308e2ffb21172792780bbad3d6905b05b8b
Xenia <markg0s@mail.ru>
2024-04-24 16:57:29 +0300

    refactor

--- app/src/main/java/com/xenia/apptosupportpatientswithocd/ui/theme/Theme.kt
+++ app/src/main/java/com/xenia/apptosupportpatientswithocd/ui/theme/Theme.kt
@@ -45,21 +45,13 @@ fun AppToSupportPatientsWithOCDTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = LightColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = Color(0xFF101018).toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            //WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
